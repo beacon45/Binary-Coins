6de417386459803e851ca74c1bb28bd71299a4ad
beacon45 <codewithbeacon@gmail.com>
2023-09-21 03:28:17 +0530

    curr-254

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -7,13 +7,13 @@ const TableComponent = () => {
 
     return (
         <>
-            <div className="flex flex-col mt-9 border border-gray-100 rounded">
+            <div className="flex flex-col mt-9 border-2 border-[#ccb94c] rounded">
                 {cryptoData ? (
                     <table className="w-full table-auto">
-                        <thead className="capitalize text-base text-[#858072] font-medium border-b border-white">
+                        <thead className="capitalize text-base text-[#858072] font-medium border-2 border-b border-[#ccbb59]">
                             <tr>
                                 <th className="py-1">Asset</th>
-                                <th className="py-1 sm:table-cell hidden">Name</th>
+                                <th className="py-1">Name</th>
                                 <th className="py-1">Price</th>
                                 <th className="py-1 sm:table-cell hidden">Total Volume</th>
                                 <th className="py-1 sm:table-cell hidden">Market Cap Change</th>
@@ -27,16 +27,50 @@ const TableComponent = () => {
                                 return (
                                     <tr
                                         key={data.id}
-                                        className="text-center text-base border-b border-gray-100 hover:bg-gray-200 last:border-b-0"
+                                        className="text-center text-white border-b font-semibold border-2 border-[#ccb94c]
+                                         hover:bg-[#dede6e] hover:text-black last:border-b-0"
                                     >
-                                        <td className='py-4'>asset</td>
+                                        <td className="py-4 flex items-center uppercase">
+                                            <img
+                                                className="w-[1.6rem] h-[1.6rem] mx-1.5"
+                                                src={data.image}
+                                                alt={data.name}
+                                            />
+                                            <span>
+                                                {data.symbol}
+                                            </span>
+                                        </td>
                                         <td className='py-4'>{data.name}</td>
-                                        <td className='py-4'>price</td>
-                                        <td className='py-4'>.totalVolume</td>
-                                        <td className='py-4'>marketCapChange</td>
-                                        <td className='py-4'>oneHourChange</td>
-                                        <td className='py-4'>twentyFourHourChange</td>
-                                        <td className='py-4'>sevenDayChange</td>
+                                        <td className='py-4 sm:text-[#4c4cdd] text-[#5ebe35] '> {new Intl.NumberFormat("en-IN", {
+                                            style: "currency",
+                                            currency: "inr",
+                                        }).format(data.current_price)}</td>
+                                        <td className='py-4 sm:table-cell hidden'>{data.total_volume}</td>
+                                        <td className={
+                                            data.market_cap_change_percentage_24h > 0
+                                                ? " text-[#5ebe35] py-4 sm:table-cell hidden "
+                                                : " text-[#da4141] py-4  sm:table-cell hidden"
+                                        }>{data.market_cap_change_percentage_24h}%</td>
+                                        <td className={
+                                            data.price_change_percentage_1h_in_currency > 0
+                                                ? " text-[#5ebe35] py-4 sm:table-cell hidden "
+                                                : " text-[#da4141] py-4  sm:table-cell hidden"
+                                        }>{Number(
+                                            data.price_change_percentage_1h_in_currency
+                                        ).toFixed(2)}</td>
+                                        <td className={
+                                            data.price_change_percentage_24h_in_currency > 0
+                                                ? " text-[#5ebe35] py-4 sm:table-cell hidden "
+                                                : " text-[#da4141] py-4  sm:table-cell hidden"
+                                        }>{Number(data.price_change_percentage_24h_in_currency
+                                        ).toFixed(2)}</td>
+                                        <td className={
+                                            data.price_change_percentage_7d_in_currency > 0
+                                                ? " text-[#5ebe35] py-4 sm:table-cell hidden "
+                                                : " text-[#da4141] py-4  sm:table-cell hidden"
+                                        }> {Number(
+                                            data.price_change_percentage_7d_in_currency
+                                        ).toFixed(2)}</td>
                                     </tr>
                                 )
                             })}
--- src/data/CryptoContext.jsx
+++ src/data/CryptoContext.jsx
@@ -9,7 +9,7 @@ export const CryptoProvider=({children})=>{
         setCryptoData();
         try {
             const data = await fetch(
-                `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+                `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
               ).then(res=>res.json()).then(json=>json);
               console.log(data)
               setCryptoData(data)
