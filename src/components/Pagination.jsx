403dede84790a7ae8e7f1aa5babd4b02c44c3140
beacon45 <codewithbeacon@gmail.com>
2023-10-07 21:49:35 +0530

    page-47-2149

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,7 +4,7 @@ import { CryptoContext } from '../data/CryptoContext';
 import PerPage from './PerPage';
 
 const Pagination = () => {
-    const {page, setPage}=useContext(CryptoContext);
+    const { page, setPage, perPage, cryptoData } = useContext(CryptoContext);
 
     const totalPage = 50;
     //next Page function
@@ -43,80 +43,86 @@ const Pagination = () => {
             setPage(page - 2);
         }
     }
-    return (
-        <>
-            <div className="flex flex-col items-center sm:flex-row sm:items-center">
-                <PerPage/>
-                <ul className="flex items-center justify-end text-base">
-                    <li className="flex items-center">
-                        <button className="outline-1 hover:bg-[#f96868] w-8" onClick={prev}>
-                            <BiSolidCaretLeftSquare className="w-full h-auto text-[#f4f447]" />
-                        </button>
-                    </li>
-                    { page + 1 === totalPage || page === totalPage?
-                    <li>
-                    <button className="ouline-1 hover:text-[#ff0000] text-[#0d0d09] bg-[#f6f63e] 
-                    rounded-md w-8 h-auto flex items-center justify-center mx-1.5 font-bold" onClick={threePrev}>
-                        . . .
-                    </button>
-                </li>
-                    : null
-                    }
-                    {page - 1 !== 0 ?
-                        <li>
-                            <button onClick={prev} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
-                    justify-center bg-[#f6f63e] mx-1.5">
-                                {page - 1}
+
+    if (cryptoData && cryptoData.length >= perPage) {
+        return (
+            <>
+                <div className="flex flex-col items-center sm:flex-row sm:items-center">
+                    <PerPage />
+                    <ul className="flex items-center justify-end text-base">
+                        <li className="flex items-center">
+                            <button className="outline-1 hover:bg-[#f96868] w-8" onClick={prev}>
+                                <BiSolidCaretLeftSquare className="w-full h-auto text-[#f4f447]" />
                             </button>
                         </li>
-                        : null
-                    }
-                    <li>
-                        <button disabled className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
+                        {page + 1 === totalPage || page === totalPage ?
+                            <li>
+                                <button className="ouline-1 hover:text-[#ff0000] text-[#0d0d09] bg-[#f6f63e] 
+                        rounded-md w-8 h-auto flex items-center justify-center mx-1.5 font-bold" onClick={threePrev}>
+                                    . . .
+                                </button>
+                            </li>
+                            : null
+                        }
+                        {page - 1 !== 0 ?
+                            <li>
+                                <button onClick={prev} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
                         justify-center bg-[#f6f63e] mx-1.5">
-                            {page}
-                        </button>
-                    </li>
-                    {page + 1 !== totalPage && page !== totalPage ?
+                                    {page - 1}
+                                </button>
+                            </li>
+                            : null
+                        }
                         <li>
-                            <button onClick={next} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
+                            <button disabled className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
+                            justify-center bg-[#f6f63e] mx-1.5">
+                                {page}
+                            </button>
+                        </li>
+                        {page + 1 !== totalPage && page !== totalPage ?
+                            <li>
+                                <button onClick={next} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
+                            justify-center bg-[#f6f63e] mx-1.5">
+                                    {page + 1}
+                                </button>
+                            </li>
+                            :
+                            null
+                        }
+                        {page + 1 !== totalPage && page !== totalPage ?
+                            <li>
+                                <button className="ouline-1 hover:text-[#ff0000] text-[#0d0d09] bg-[#f6f63e] 
+                        rounded-md w-8 h-auto flex items-center justify-center mx-1.5 font-bold" onClick={threeNext}>
+                                    . . .
+                                </button>
+                            </li>
+                            :
+                            null
+                        }
+                        {page !== totalPage ?
+                            <li>
+                                <button onClick={() => {
+                                    setPage(totalPage);
+                                }} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
                         justify-center bg-[#f6f63e] mx-1.5">
-                                {page + 1}
+                                    {totalPage}
+                                </button>
+                            </li>
+                            : null
+                        }
+                        <li className="flex items-center">
+                            <button className="outline-1 hover:bg-[#f96868] w-8 " onClick={next}>
+                                <BiSolidCaretRightSquare className="w-full h-auto text-[#f4f447]" />
                             </button>
                         </li>
-                        :
-                        null
-                    }
-                    {page + 1 !== totalPage && page !== totalPage ?
-                        <li>
-                            <button className="ouline-1 hover:text-[#ff0000] text-[#0d0d09] bg-[#f6f63e] 
-                    rounded-md w-8 h-auto flex items-center justify-center mx-1.5 font-bold" onClick={threeNext}>
-                                . . .
-                            </button>
-                        </li>
-                        :
-                        null
-                    }
-                    {page !== totalPage ?
-                        <li>
-                            <button onClick={() => {
-                                setPage(totalPage);
-                            }} className="ouline-1 hover:text-[red] rounded-md w-8 h-auto flex items-center 
-                    justify-center bg-[#f6f63e] mx-1.5">
-                                {totalPage}
-                            </button>
-                        </li>
-                        : null
-                    }
-                    <li className="flex items-center">
-                        <button className="outline-1 hover:bg-[#f96868] w-8 " onClick={next}>
-                            <BiSolidCaretRightSquare className="w-full h-auto text-[#f4f447]" />
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
+                    </ul>
+                </div>
+            </>
+        )
+    }
+    else {
+        return null;
+    }
 }
 
 export default Pagination
