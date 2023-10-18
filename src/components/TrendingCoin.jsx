bdf7011650b572173ca268261040c0fa3df4ddad
beacon45 <codewithbeacon@gmail.com>
2023-10-19 01:12:16 +0530

    trendCom-0112

---
+++ src/components/TrendingCoin.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const TrendingCoin = ({ data }) => {
+    let navigate = useNavigate();
+
+    const getCoinDetails = (id) => {
+        navigate(`${id}`);
+    };
+    return (
+        <>
+            <div className=" w-full sm:w-[40%] bg-[#5f5f5a] mb-12
+    last:mb-0 rounded-lg p-4 relative cursor-pointer
+    hover:bg-[#8e856e] hover:bg-opacity-40 text-[#e8e5c4] flex flex-col mx-5
+    "
+                onClick={() => getCoinDetails(data.id)}
+            >{
+                    data ?
+                        (
+                            <>
+                                <h3 className="text-base flex items-center my-0.5">
+                                    <span>Name:&nbsp; </span>
+                                    <span className=' text-[#eeee58]'>{data.name}</span>
+                                    <img
+                                        src={data.small}
+                                        alt={data.name}
+                                        className="w-[1.5rem] h-[1.5rem] mx-1.5 rounded-full"
+                                    />
+                                </h3>
+                                <h3 className="text-base flex items-center my-0.5">
+                                    <span>Market Cap Rank:&nbsp; </span>
+                                    <span className=' text-[#eeee58]'>{data.market_cap_rank}</span>
+                                </h3>
+                                <h3 className="text-base flex items-center my-0.5">
+                                    <span>Price:&nbsp; </span>
+                                    <span className=' text-[#eeee58]'>
+                                        {new Intl.NumberFormat("en-IN", {
+                                            style: "currency",
+                                            currency: "btc",
+                                            maximumSignificantDigits: 5,
+                                        }).format(data.price_btc)}</span>
+                                </h3>
+                                <h3 className="text-base flex items-center my-0.5">
+                                    <span>Score:&nbsp; </span>
+                                    <span className=' text-[#eeee58]'>{data.score}</span>
+                                </h3>
+                                <img
+                                    src={data.large}
+                                    alt={data.name}
+                                    className="w-[35%] h-auto rounded-full absolute top-2/4 -right-12 -translate-y-2/4"
+                                />
+                            </>
+                        )
+                        : (
+                            <div
+                                className="w-full  h-full flex justify-center items-center
+                                 "
+                            >
+                                <div
+                                    className="w-8 h-8 border-4 border-[#e9d949] rounded-full
+                                 border-b-gray-200 animate-spin 
+                                 "
+                                    role="status"
+                                />
+                                <span className="ml-2">please wait...</span>
+                            </div>
+                        )
+                }
+
+            </div>
+        </>
+    )
+}
+
+export default TrendingCoin
--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TrendingContext } from '../data/TrendingContext'
+import { Outlet } from 'react-router-dom';
+import TrendingCoin from '../components/TrendingCoin';
+import { FiSlack } from "react-icons/fi";
 
 const Trending = () => {
+  const { trendData, resetTrendingResult } = useContext(TrendingContext);
   return (
-    <div>Trending</div>
+    <>
+      <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+        <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly border border-[#ccb94c] rounded" >
+          { trendData && trendData.map((coin)=>(
+            <TrendingCoin key={coin.coin_id} data={coin.item}/>
+          ))}
+          <button className="w-[2rem] ml-4 hover:scale-110 transition-all transition-ease
+           absolute right-0 -top-10"
+          onClick={resetTrendingResult}>
+            <FiSlack size={25} className=' fill-[#f3f360]'/>
+          </button>
+        </div>
+        <Outlet/>
+      </section>
+    </>
   )
 }
 
